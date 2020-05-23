85d4638ec7347bcfececcb7efd8724da0e5a97df
Yeun SeungHoo <yysh123@gmail.com>
2020-05-23 14:14:28 +0900

    1 . 게임 오브젝트
    - 태그를 사용하여 레이아웃을 변경하면 카메라의 컬링마스크를 적용하여 카메라에서 안보이게 할 수 있다.
    - 인스펙터의 체크를 이용하여 비·활성화 할 수 있다.

---
+++ Unity_Tutorial/Assets/Scripts/Test.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    Vector3 rotation;
+
+    [SerializeField]
+    private GameObject go_camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = this.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            ////W클릭시 Z축 값 증가
+            //this.transform.position = this.transform.position + new Vector3(0, 0, 1) * Time.deltaTime;
+            //this.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime);
+
+            ////eulerAngles를 사용한 것
+            ////rotation = rotation + new Vector3(90, 0, 0) * Time.deltaTime;
+            ////this.transform.eulerAngles = rotation;
+            ////Debug.Log(transform.eulerAngles);
+
+            ////로테이트를 사용한 것 (위와 동일하다)
+            ////this.transform.Rotate(new Vector3(90, 0, 0) * Time.deltaTime);
+
+            //rotation = rotation + new Vector3(90, 0, 0) * Time.deltaTime;
+            //this.transform.rotation = Quaternion.Euler(rotation);
+
+            //크기 증가
+            //this.transform.localScale = this.transform.localScale + new Vector3(2, 2, 2) * Time.deltaTime;
+
+            //this.transform.LookAt(go_camera.transform.position);
+
+
+        }
+
+        transform.RotateAround(go_camera.transform.position, Vector3.up, 100 * Time.deltaTime);
+    }
+}
