5492e4cd8e5c7f010d736f52a6edc4d014db8f87
Yeun SeungHoo <yysh123@gmail.com>
2020-05-23 23:53:48 +0900

    8. 애니메이션
    animator - 수많은 애니메이션을 제어할 수 있게 해줌
    
    play - 재생
    playQueued - 재생후 나오는 애니매이션
    Blend - 같이 재생 (섞어서)
    CorssFade - 자연러운 애니매이션 교체
    isPlaying - 특정 애니매이션 실행중 지칭할때

---
+++ Unity_Tutorial/Assets/Scripts/Test_T8.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_T8 : MonoBehaviour
+{
+    private Animation anim;
+
+    private AnimationClip clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //anim.Play("Cube_2");
+            //anim.PlayQueued("Cube_2");
+            //anim.Blend("Cube_2");
+            //anim.CrossFade("Cube_2");
+            anim.clip = clip;
+        }
+    }
+}
