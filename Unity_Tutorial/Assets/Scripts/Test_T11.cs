fe751857fc732fff90d2f4271b7b38adc16c9108
Yeun SeungHoo <yysh123@gmail.com>
2020-05-24 12:54:54 +0900

    11. UI

---
+++ Unity_Tutorial/Assets/Scripts/Test_T11.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_T11 : MonoBehaviour
+{
+    [SerializeField] private Text txt_name;
+    [SerializeField] private Image img_name;
+    [SerializeField] private Sprite sprite;
+
+    private bool isCoolTime = false;
+
+    private float currentTime = 1f;
+    private float delayTime = 5f;
+
+    private void Update()
+    {
+
+        img_name.color = Color.red;
+
+        if (isCoolTime)
+        {
+            currentTime -= Time.deltaTime;
+            img_name.fillAmount = currentTime / delayTime;
+
+            if (currentTime <= 0)
+            {
+                isCoolTime = false;
+                currentTime = delayTime;
+                img_name.fillAmount = currentTime;
+            }
+        }
+    }
+
+    public void Change()
+    {
+        txt_name.text = "변경됨";
+        isCoolTime = true;
+    }
+
+}
