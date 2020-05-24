f1f3eafb9165d6a93bfa6ec964115183254fb186
Yeun SeungHoo <yysh123@gmail.com>
2020-05-24 15:55:11 +0900

    17. Physics 클래스

---
+++ Unity_Tutorial/Assets/Scripts/Test_T17.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_T17 : MonoBehaviour
+{
+    private float createTime = 1f;
+    private float currentCreateTime;
+
+    [SerializeField] private GameObject go_BulletPrefab;
+
+
+    private void Update()
+    {
+        Collider[] col = Physics.OverlapSphere(transform.position, 5f);
+
+        if(col.Length>0)
+        {
+            for(int i =0; i< col.Length; i++)
+            {
+                Transform tf_Target = col[i].transform;
+
+                if(tf_Target.tag == "Player")
+                {
+                    Quaternion rotation = Quaternion.LookRotation(tf_Target.position - this.transform.position);
+                    transform.rotation = rotation;
+
+                    currentCreateTime += Time.deltaTime;
+
+                    if(currentCreateTime >= createTime)
+                    {
+                        Instantiate(go_BulletPrefab, transform.position, rotation);
+                        currentCreateTime = 0;
+                    }
+                }
+            }
+        }
+    }
+}
